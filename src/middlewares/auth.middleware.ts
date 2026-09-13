9e97717b3f2598a269f2960c20faf2dd6ea05e5d
ParthSingh870 <hp2024jlp@gmail.com>
2026-09-13 19:35:55 +0530

    Initial Commit

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -36,9 +36,6 @@ export const authenticate = (
     }
 };
 
-
-
-
 export const authorize = (...allowedRoles: string[]) => {
     return (req: AuthRequest, res: Response, next: NextFunction) => {
         if (!req.user || !allowedRoles.includes(req.user.role)) {
